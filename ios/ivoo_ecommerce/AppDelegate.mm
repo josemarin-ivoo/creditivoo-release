ef3cd15fbaef90f6c29a62b31e81035e0de27a7c
José Marin <jose.marin@SIRAGON.CORP>
2025-12-31 09:06:50 -0400

    AppDelegate.mm adaptado

--- ios/ivoo_ecommerce/AppDelegate.mm
+++ ios/ivoo_ecommerce/AppDelegate.mm
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+// 1. Importaciones esenciales
+#import <Firebase.h> 
 #import <React/RCTBundleURLProvider.h>
 #import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 
@@ -7,23 +9,35 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // 2. Configuración de Firebase (Debe ir antes de super)
   [FIRApp configure];
+
+  // 3. Configuración de React Native
   self.moduleName = @"ivoo";
   self.dependencyProvider = [RCTAppDependencyProvider new];
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
+  
+  // Props iniciales personalizadas
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+// 4. MÉTODO CRUCIAL: Define dónde está el código JavaScript
+- (NSURL *)bundleURL
 {
 #if DEBUG
+  // En desarrollo, busca el servidor Metro (localhost:8081)
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
+  // En producción (Release), busca el archivo main.jsbundle compilado
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+// 5. MÉTODO DE COMPATIBILIDAD: Para versiones que aún usan el Bridge
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
 }
 
 @end
